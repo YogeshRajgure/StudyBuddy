ee640e8861d70212bd433f7b656e596b2389a0de
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2023-01-16 01:44:32 +0530

    All process before setting up database

--- studybud_dj/base/views.py
+++ studybud_dj/base/views.py
@@ -3,11 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
+rooms = [
+    {'id':1, 'name': 'Lets learn Python!'},
+    {'id':2, 'name': 'Lets learn SQL!'},
+    {'id':3, 'name': 'Lets learn Django!'},
+]
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    print(context)
+    return render(request, 'base/room.html', context)
