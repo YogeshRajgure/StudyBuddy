0ee4e6f9a8ad9979f2016e50227f75802f543014
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2023-01-17 01:18:58 +0530

    connected database

--- studybud_dj/base/models.py
+++ studybud_dj/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    #host=
+    #topic=
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    #participants = 
+    updated = models.DateTimeField(auto_now=True) # takes snap everytmie
+    created = models.DateTimeField(auto_now_add=True) # takes snap at first time
+
+    def __str__(self):
+        return self.name
+
+
